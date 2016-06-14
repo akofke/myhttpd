07e876b15b67cefa82ff532fdef76741f8528720
Ehab Mahran <ehabmahr@buffalo.edu>
2016-06-14 20:36:41 +0000

    handlejob.c for thread(s) that handle the actual html request

---
+++ src/handlejob.c
@@ -0,0 +1,8 @@
+//Code for the thread(s) that handle the HTML request as given by the selector thread
+
+#include <myhttpd.h>
+
+int handlejob(char request[]){
+	//Code for handling a given html request
+	return 0;
+}
