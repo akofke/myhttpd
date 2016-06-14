0b669886c582bf85d3a3374c70ca0f852c9aebad
Ehab Mahran <ehabmahr@buffalo.edu>
2016-06-14 20:35:38 +0000

    selectthread.c thread for thread switching

---
+++ src/selectthread.c
@@ -0,0 +1,12 @@
+//Code for the thread that selects a handler thread to do a job after a job was received from the listener thread
+
+#include <myhttpd.h>
+#include <queue>
+
+int selectorthread(int numthread, bool sorttype){ // sorttype: true for FCFS, false for SJF
+	int curthreads = 0;
+	if (sorttype){
+
+	}
+	return 0;
+}
