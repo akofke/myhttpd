7edad49d78e9515e83569c7a96ae131ad48a78d8
Alex Kofke <akofke@gmail.com>
2016-06-15 14:40:47 -0400

    added usage function
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <ctype.h>
 
+void usage(int is_err);
+
 int main(int argc, char *argv[]) {
     int c;
     while((c = getopt(argc, argv, "dhl:r:t:n:s:")) != -1) {
@@ -12,6 +14,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 'h':
                 // help, usage
+                usage(0);
                 break;
             case 'l':
                 // logging
@@ -33,6 +36,7 @@ int main(int argc, char *argv[]) {
                 break;
             case '?':
                 // handle bad options
+                usage(1);
                 break;
             default:
                 // something went very wrong if we are here
@@ -41,4 +45,16 @@ int main(int argc, char *argv[]) {
         }
 
     }
+}
+
+void usage(int is_err) {
+    /* prints usage and help. Prints to stdout or stderr depending on if its invoked
+     * as an error or from -h.
+     * TODO: add more help
+     */
+    char *msg = "Usage: myhttpd "
+        "[-d] [-h] [-l file] [-p port] [-r dir] [-t time] [-n threadnum] [-s sched]"; 
+
+    fprintf((is_err) ? stderr : stdout, "%s\n", msg);
+    exit(is_err);
 }
