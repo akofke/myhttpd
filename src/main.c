f18b3540d9e5f02e2d8a062e0aa1ac18ab92b8a3
Alex Kofke <akofke@gmail.com>
2016-06-15 01:29:39 -0400

    set up option-parsing skeleton
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello world!\n");
+    int c;
+    while((c = getopt(argc, argv, "dhl:r:t:n:s:")) != -1) {
+        switch(c) {
+            case 'd':
+                // debugging mode
+                break;
+            case 'h':
+                // help, usage
+                break;
+            case 'l':
+                // logging
+                break;
+            case 'p':
+                // port
+                break;
+            case 'r':
+                // root directory
+                break;
+            case 't':
+                // queuing time
+                break;
+            case 'n':
+                // number of worker threads
+                break;
+            case 's':
+                // scheduling policy
+                break;
+            case '?':
+                // handle bad options
+                break;
+            default:
+                // something went very wrong if we are here
+                abort();
+
+        }
+
+    }
 }
