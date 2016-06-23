8c75ed21a46a546c02935e06368e93b72dcae935
Alex Kofke <akofke@gmail.com>
2016-06-23 02:10:51 -0400

    reached basic layout of server code
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- src/server.c
+++ src/server.c
@@ -5,6 +5,12 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#define BACKLOG 20
+#define BUF_SIZE 1024
+#define RECV_FLAGS 0
+
+void serve_connections(int sockfd);
+
 void get_server_addrinfo(char *port, struct addrinfo *info) {
     struct addrinfo hints;
     int status;
@@ -27,25 +33,79 @@ void get_server_addrinfo(char *port, struct addrinfo *info) {
     }
 }
 
-int create_socket(struct addrinfo *info);
+/*
+ * Setup server to accept connections
+ */
+void setup_server(char *port) {
+    // get local address info
+    struct addrinfo *info;
+    get_server_addrinfo(port, info);
+
+    /* create a socket.
+     * socket() call takes the domain (PF_INET), the socket type (SOCK_STREAM),
+     * and the protocol (redundant; PF_INET or 0)
+     */
+    int sockfd;
+    if((sockfd = socket(info->ai_family, info->ai_socktype, info->ai_protocol)) < 0) {
+        perror("error getting socket file descriptor");
+        exit(1);
+    }
+
+    // bind to socket
+    if(bind(sockfd, info->ai_addr, info->ai_addrlen) == -1) {
+        perror("error binding to socket");
+        exit(1);
+    }
+
+    int yes = 1;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+        perror("error setting socket option");
+        exit(1);
+    }
+
+    freeaddrinfo(info);
+    serve_connections(sockfd);
+
+}
 
 /*
  * Main server loop
  */
-void serve(char *port) {
-    // get local address info
-    struct addrinfo *info;
-    get_server_addrinfo(port, info);
-    
-    // create a socket
+void serve_connections(int sockfd) {
 
-    // bind to socket
-    
-    // listen on socket
+    while(1) {
+        // listen on socket
+        if(listen(sockfd, BACKLOG) == -1) {
+            perror("error listening on socket");
+            exit(1);
+        }
 
-    // accept incoming connection
+        // accept incoming connection
+        struct sockaddr_storage remote_addr;
+        socklen_t addr_size = sizeof remote_addr;
+        int accepted_fd;
+        if((accepted_fd = accept(sockfd, (struct sockaddr *)&remote_addr, &addr_size))) {
+            perror("error accepting connection");
+            exit(1);
+        }
 
-    // read from connection
+        // read from connection
+        char recv_buf[BUF_SIZE];
+        int recv_retval;
+        if((recv_retval = recv(accepted_fd, recv_buf, BUF_SIZE, RECV_FLAGS)) == -1) {
+            perror("error while recieving from client");
+            exit(1);
+        } else if(recv_retval == 0) {
+            // TODO: probably want to implement some other behavior here
+            fprintf(stderr, "client has closed the connection");
+            continue;
+        }
+
+        // debug
+        printf("%s\n", recv_buf);
+
+    }
+
 }
 
 
